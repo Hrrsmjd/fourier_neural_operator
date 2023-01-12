dea3b4987ee0f9330360f0ffc23551630454a3e1
Hrrsmjd <hrrsmjd@gmail.com>
2023-01-12 11:50:28 +0000

    Updated 1D Fourier Integral Operator

--- fourier_1d.py
+++ fourier_1d.py
@@ -3,6 +3,7 @@
 This file is the Fourier Neural Operator for 1D problem such as the (time-independent) Burgers equation discussed in Section 5.1 in the [paper](https://arxiv.org/pdf/2010.08895.pdf).
 """
 
+
 import torch.nn.functional as F
 from timeit import default_timer
 from utilities3 import *
@@ -12,42 +13,59 @@ np.random.seed(0)
 
 
 ################################################################
-#  1d fourier layer
+#  1d Fourier Integral Operator
 ################################################################
 class SpectralConv1d(nn.Module):
-    def __init__(self, in_channels, out_channels, modes1):
+    def __init__(self, in_channels: int, out_channels: int, modes: int):
         super(SpectralConv1d, self).__init__()
-
         """
-        1D Fourier layer. It does FFT, linear transform, and Inverse FFT.    
+        Initializes the 1D Fourier layer. It does FFT, linear transform, and Inverse FFT.
+        Args:
+            in_channels (int): input channels to the FNO layer
+            out_channels (int): output channels of the FNO layer
+            modes (int): number of Fourier modes to multiply, at most floor(N/2) + 1
         """
-
         self.in_channels = in_channels
         self.out_channels = out_channels
-        self.modes1 = modes1  #Number of Fourier modes to multiply, at most floor(N/2) + 1
-
+        self.modes = modes
         self.scale = (1 / (in_channels*out_channels))
-        self.weights1 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, dtype=torch.cfloat))
+        self.weights = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes, dtype=torch.cfloat)) ## Tensor
 
     # Complex multiplication
     def compl_mul1d(self, input, weights):
-        # (batch, in_channel, x ), (in_channel, out_channel, x) -> (batch, out_channel, x)
+        """
+        Complex multiplication of the Fourier modes.
+        [batch, in_channels, x], [in_channel, out_channels, x] -> [batch, out_channels, x]
+            Args:
+                input (torch.Tensor): input tensor of size [batch, in_channels, x]
+                weights (torch.Tensor): weight tensor of size [in_channels, out_channels, x]
+            Returns:
+                torch.Tensor: output tensor with shape [batch, out_channels, x]
+        """
         return torch.einsum("bix,iox->box", input, weights)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Fourier transformation, multiplication of relevant Fourier modes, backtransformation
+        Args:
+            x (torch.Tensor): input to forward pass os shape [batch, in_channels, x]
+        Returns:
+            torch.Tensor: output of size [batch, out_channels, x]
+        """
         batchsize = x.shape[0]
-        #Compute Fourier coeffcients up to factor of e^(- something constant)
+        # Fourier transformation
         x_ft = torch.fft.rfft(x)
 
         # Multiply relevant Fourier modes
         out_ft = torch.zeros(batchsize, self.out_channels, x.size(-1)//2 + 1,  device=x.device, dtype=torch.cfloat)
-        out_ft[:, :, :self.modes1] = self.compl_mul1d(x_ft[:, :, :self.modes1], self.weights1)
+        out_ft[:, :, :self.modes] = self.compl_mul1d(x_ft[:, :, :self.modes], self.weights)
 
         #Return to physical space
         x = torch.fft.irfft(out_ft, n=x.size(-1))
         return x
 
-class MLP(nn.Module):
+
+class MLP(nn.Module): ##TODO: Rewrite
     def __init__(self, in_channels, out_channels, mid_channels):
         super(MLP, self).__init__()
         self.mlp1 = nn.Conv1d(in_channels, mid_channels, 1)
@@ -59,6 +77,10 @@ class MLP(nn.Module):
         x = self.mlp2(x)
         return x
 
+
+################################################################
+#  1d Fourier Network
+################################################################
 class FNO1d(nn.Module):
     def __init__(self, modes, width):
         super(FNO1d, self).__init__()
@@ -81,6 +103,7 @@ class FNO1d(nn.Module):
         self.padding = 8 # pad the domain if input is non-periodic
 
         self.p = nn.Linear(2, self.width) # input channel_dim is 2: (u0(x), x)
+
         self.conv0 = SpectralConv1d(self.width, self.width, self.modes1)
         self.conv1 = SpectralConv1d(self.width, self.width, self.modes1)
         self.conv2 = SpectralConv1d(self.width, self.width, self.modes1)
@@ -93,6 +116,7 @@ class FNO1d(nn.Module):
         self.w1 = nn.Conv1d(self.width, self.width, 1)
         self.w2 = nn.Conv1d(self.width, self.width, 1)
         self.w3 = nn.Conv1d(self.width, self.width, 1)
+
         self.q = MLP(self.width, 1, self.width*2)  # output channel_dim is 1: u1(x)
 
     def forward(self, x):
@@ -136,6 +160,78 @@ class FNO1d(nn.Module):
         gridx = gridx.reshape(1, size_x, 1).repeat([batchsize, 1, 1])
         return gridx.to(device)
 
+# class FNO1d(nn.Module):
+#     def __init__(self, modes, width, time_future, time_history):
+#         super(FNO1d, self).__init__()
+
+#         """
+#         The overall network. It contains 4 layers of the Fourier layer.
+#         1. Lift the input to the desire channel dimension by self.fc0 .
+#         2. 4 layers of the integral operators u' = (W + K)(u).
+#             W defined by self.w; K defined by self.conv .
+#         3. Project from the channel space to the output space by self.fc1 and self.fc2 .
+
+#         input: a driving function observed at T timesteps + 1 locations (u(1, x), ..., u(T, x),  x).
+#         input shape: (batchsize, x=s, c=2)
+#         output: the solution of a later timestep
+#         output shape: (batchsize, x=s, c=1)
+#         """
+#         self.modes = modes
+#         self.width = width
+#         self.time_future = time_future
+#         self.time_history = time_history
+#         self.fc0 = nn.Linear(self.time_history+1, self.width)
+
+#         self.conv0 = SpectralConv1d(self.width, self.width, self.modes)
+#         self.conv1 = SpectralConv1d(self.width, self.width, self.modes)
+#         self.conv2 = SpectralConv1d(self.width, self.width, self.modes)
+#         self.conv3 = SpectralConv1d(self.width, self.width, self.modes)
+#         self.w0 = nn.Conv1d(self.width, self.width, 1)
+#         self.w1 = nn.Conv1d(self.width, self.width, 1)
+#         self.w2 = nn.Conv1d(self.width, self.width, 1)
+#         self.w3 = nn.Conv1d(self.width, self.width, 1)
+
+#         self.fc1 = nn.Linear(self.width, 128)
+#         self.fc2 = nn.Linear(128, self.time_future)
+
+    # def forward(self, u):
+    #     grid = self.get_grid(u.shape, u.device)
+    #     x = torch.cat((u, grid), dim=-1)
+    #     x = self.fc0(x)
+    #     x = x.permute(0, 2, 1)
+
+    #     x1 = self.conv0(x)
+    #     x2 = self.w0(x)
+    #     x = x1 + x2
+    #     x = F.gelu(x)
+
+    #     x1 = self.conv1(x)
+    #     x2 = self.w1(x)
+    #     x = x1 + x2
+    #     x = F.gelu(x)
+
+    #     x1 = self.conv2(x)
+    #     x2 = self.w2(x)
+    #     x = x1 + x2
+    #     x = F.gelu(x)
+
+    #     x1 = self.conv3(x)
+    #     x2 = self.w3(x)
+    #     x = x1 + x2
+
+    #     x = x.permute(0, 2, 1)
+    #     x = self.fc1(x)
+    #     x = F.gelu(x)
+    #     x = self.fc2(x)
+    #     return x
+
+    # def get_grid(self, shape, device):
+    #     batchsize, size_x = shape[0], shape[1]
+    #     gridx = torch.tensor(np.linspace(0, 1, size_x), dtype=torch.float)
+    #     gridx = gridx.reshape(1, size_x, 1).repeat([batchsize, 1, 1])
+    #     return gridx.to(device)
+
+
 ################################################################
 #  configurations
 ################################################################
@@ -148,7 +244,8 @@ s = h
 
 batch_size = 20
 learning_rate = 0.001
-epochs = 500
+# epochs = 500
+epochs = 2
 iterations = epochs*(ntrain//batch_size)
 
 modes = 16
@@ -161,7 +258,9 @@ width = 64
 # Data is of the shape (number of samples, grid size)
 dataloader = MatReader('data/burgers_data_R10.mat')
 x_data = dataloader.read_field('a')[:,::sub]
+# print(x_data.shape)
 y_data = dataloader.read_field('u')[:,::sub]
+# print(y_data.shape)
 
 x_train = x_data[:ntrain,:]
 y_train = y_data[:ntrain,:]
@@ -171,70 +270,75 @@ y_test = y_data[-ntest:,:]
 x_train = x_train.reshape(ntrain,s,1)
 x_test = x_test.reshape(ntest,s,1)
 
-train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_train, y_train), batch_size=batch_size, shuffle=True)
-test_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_test, y_test), batch_size=batch_size, shuffle=False)
+# train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_train, y_train), batch_size=batch_size, shuffle=True)
+# test_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_test, y_test), batch_size=batch_size, shuffle=False)
 
-# model
-model = FNO1d(modes, width).cuda()
-print(count_params(model))
+# # model
+# # model = FNO1d(modes, width).cuda()
+# model = FNO1d(modes, width).cpu()
+# print(count_params(model))
 
-################################################################
-# training and evaluation
-################################################################
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-4)
-scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=iterations)
+# ################################################################
+# # training and evaluation
+# ################################################################
+# optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-4)
+# scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=iterations)
 
-myloss = LpLoss(size_average=False)
-for ep in range(epochs):
-    model.train()
-    t1 = default_timer()
-    train_mse = 0
-    train_l2 = 0
-    for x, y in train_loader:
-        x, y = x.cuda(), y.cuda()
+# myloss = LpLoss(size_average=False)
+# for ep in range(epochs):
+#     model.train()
+#     t1 = default_timer()
+#     train_mse = 0
+#     train_l2 = 0
+#     for x, y in train_loader:
+#         # x, y = x.cuda(), y.cuda()
+#         x, y = x.cpu(), y.cpu()
 
-        optimizer.zero_grad()
-        out = model(x)
+#         optimizer.zero_grad()
+#         out = model(x)
 
-        mse = F.mse_loss(out.view(batch_size, -1), y.view(batch_size, -1), reduction='mean')
-        l2 = myloss(out.view(batch_size, -1), y.view(batch_size, -1))
-        l2.backward() # use the l2 relative loss
+#         mse = F.mse_loss(out.view(batch_size, -1), y.view(batch_size, -1), reduction='mean')
+#         l2 = myloss(out.view(batch_size, -1), y.view(batch_size, -1))
+#         l2.backward() # use the l2 relative loss
 
-        optimizer.step()
-        scheduler.step()
-        train_mse += mse.item()
-        train_l2 += l2.item()
+#         optimizer.step()
+#         scheduler.step()
+#         train_mse += mse.item()
+#         train_l2 += l2.item()
 
-    model.eval()
-    test_l2 = 0.0
-    with torch.no_grad():
-        for x, y in test_loader:
-            x, y = x.cuda(), y.cuda()
+#     model.eval()
+#     test_l2 = 0.0
+#     with torch.no_grad():
+#         for x, y in test_loader:
+#             # x, y = x.cuda(), y.cuda()
+#             x, y = x.cpu(), y.cpu()
 
-            out = model(x)
-            test_l2 += myloss(out.view(batch_size, -1), y.view(batch_size, -1)).item()
+#             out = model(x)
+#             test_l2 += myloss(out.view(batch_size, -1), y.view(batch_size, -1)).item()
 
-    train_mse /= len(train_loader)
-    train_l2 /= ntrain
-    test_l2 /= ntest
+#     train_mse /= len(train_loader)
+#     train_l2 /= ntrain
+#     test_l2 /= ntest
 
-    t2 = default_timer()
-    print(ep, t2-t1, train_mse, train_l2, test_l2)
+#     t2 = default_timer()
+#     print(ep, t2-t1, train_mse, train_l2, test_l2)
 
-# torch.save(model, 'model/ns_fourier_burgers')
-pred = torch.zeros(y_test.shape)
-index = 0
-test_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_test, y_test), batch_size=1, shuffle=False)
-with torch.no_grad():
-    for x, y in test_loader:
-        test_l2 = 0
-        x, y = x.cuda(), y.cuda()
+# # torch.save(model, 'model/ns_fourier_burgers')
+# pred = torch.zeros(y_test.shape)
+# index = 0
+# test_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_test, y_test), batch_size=1, shuffle=False)
+# with torch.no_grad():
+#     for x, y in test_loader:
+#         test_l2 = 0
+#         # x, y = x.cuda(), y.cuda()
+#         x, y = x.cpu(), y.cpu()
 
-        out = model(x).view(-1)
-        pred[index] = out
+#         out = model(x).view(-1)
+#         pred[index] = out
 
-        test_l2 += myloss(out.view(1, -1), y.view(1, -1)).item()
-        print(index, test_l2)
-        index = index + 1
+#         test_l2 += myloss(out.view(1, -1), y.view(1, -1)).item()
+#         print(index, test_l2)
+#         index = index + 1
 
 # scipy.io.savemat('pred/burger_test.mat', mdict={'pred': pred.cpu().numpy()})
+# np.savetxt('pred/burger_test.csv', pred.cpu().numpy(), delimiter=",")
